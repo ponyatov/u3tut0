3951c2b4151c8603d9140fdb782abddd31621475
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-19 15:58:06 +0400

    +
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ tut1/Assets/Scripts/boxdrop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxdrop : MonoBehaviour {
+
+	public Vector3 drift;
+
+	void Update(){
+		transform.Translate (drift*Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision coll) {
+		Debug.Log (coll.gameObject);
+	}
+
+}
---
+++ tut1/Assets/Scripts/generator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generator : MonoBehaviour {
+
+	public GameObject elem;
+
+	void Start(){
+		GameObject inst = new GameObject();
+
+		inst = Instantiate(elem,transform.position,transform.rotation) as GameObject;
+	}
+	
+}
